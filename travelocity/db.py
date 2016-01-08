1418332d4fa27ff6d71e912f3101a69934ec2d67
chenxiaojing <chxj1992@gmail.com>
2016-01-08 22:08:09 +0800

    save

--- travelocity/db.py
+++ travelocity/db.py
@@ -1,13 +1,13 @@
 import MySQLdb
 
 def save(data):
-    sql = 'REPLACE INTO itineraries(itinerary_id, title, ship_name, duration, departure_port, departure_time, \
+    sql = 'REPLACE INTO travelocity_itineraries(itinerary_id, title, ship_name, duration, departure_port, departure_time, \
             inside, oceanview, balcony, suite, is_lowest_price) VALUES ' + ','.join(data);  
     execute(sql)
 
 def execute(sql):
     try:
-        conn=MySQLdb.connect(host='localhost', user='root', passwd='87822971', db='travelocity', port=3306)
+        conn=MySQLdb.connect(host='localhost', user='root', passwd='87822971', db='apples_data_center', port=3306)
         cur=conn.cursor()
         res = cur.execute(sql)
         conn.commit();
