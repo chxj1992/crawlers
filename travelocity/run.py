169f790e30f4a957b72d0e77575642908de4bc67
chenxiaojing <chxj1992@gmail.com>
2016-01-09 07:25:52 +0800

    update

--- travelocity/run.py
+++ travelocity/run.py
@@ -1,27 +1,27 @@
-import crawl
-import urllib2
 from multiprocessing import Process
-import os
+
+import crawl
 
 WORKER_NUM = 20
 
 c = crawl.Crawl()
 
-def worker(i, step): 
-    print( "worker : " + str(i) + " start ...")
-    while True :
+
+def worker(i, step):
+    print("worker : " + str(i) + " start ...")
+    while True:
         try:
-            if (c.getListPage(i) == False) :
+            if not c.getListPage(i):
                 break
-        except :
+        except:
             print("url error caught!")
             continue
         i += step
         print 'page ' + str(i)
-    print( "worker : " + str(i) + " end.")
+    print("worker : " + str(i) + " end.")
+    exit()
 
 
 for i in range(0, WORKER_NUM):
-    p = Process(target = worker, args = (i, WORKER_NUM,))
+    p = Process(target=worker, args=(i, WORKER_NUM,))
     p.start()
-
