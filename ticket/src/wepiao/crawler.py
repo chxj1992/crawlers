a6ea2099f394e5578c8cc9d1c43f2a109e8d152b
chenxiaojing <chxj1992@gmail.com>
2016-05-07 12:14:49 +0800

    update

--- ticket/src/wepiao/crawler.py
+++ ticket/src/wepiao/crawler.py
@@ -128,11 +128,11 @@ def run():
     phone = '15669207848'
     address = '华西坝大学路12号'
     name = '陈先生'
-    ticket_number = 6
+    ticket_number = 4
     show_id = '14569213092998589'
     online_id = '780aa9a6ec0a489bb0e78588cb6d5cbd'
 
-    for i in range(1, 6):
+    for i in range(1, 11):
         print 'worker ' + str(i) + ' start ...'
         p = Process(target=order_without_area, args=(cookie, show_id, online_id, phone, address, name, ticket_number))
         p.start()
