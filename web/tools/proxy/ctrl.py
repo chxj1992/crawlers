f5a575095b4324c57f752ee10d09d73a4fd93ce5
chenxiaojing <chxj1992@gmail.com>
2016-05-12 11:15:31 +0800

    update proxy tools

--- web/tools/proxy/ctrl.py
+++ web/tools/proxy/ctrl.py
@@ -8,6 +8,21 @@ from tools.proxy import crawler
 proxy = Module(__name__, 'proxy')
 
 
-@proxy.route("/proxy/shuffle")
-def shuffle():
-    return crawler.shuffle()
+@proxy.route("/proxy/samair")
+def samair():
+    return json.dumps(crawler.samair())
+
+
+@proxy.route("/proxy/samair/shuffle")
+def samair_shuffle():
+    return json.dumps(crawler.shuffle(crawler.samair()))
+
+
+@proxy.route("/proxy/sslproxies")
+def sslproxies():
+    return json.dumps(crawler.ssl_proxies())
+
+
+@proxy.route("/proxy/sslproxies/shuffle")
+def sslproxies_shuffle():
+    return json.dumps(crawler.shuffle(crawler.ssl_proxies()))
