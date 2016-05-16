8d8ac4406e8de6a4f730a258e2945f8ba82d16e5
chenxiaojing <chxj1992@gmail.com>
2016-05-16 13:17:38 +0800

    update

--- web/tools/proxy/ctrl.py
+++ web/tools/proxy/ctrl.py
@@ -10,31 +10,49 @@ proxy = Module(__name__, 'proxy')
 
 @proxy.route("/proxy/samair")
 def samair():
-    return json.dumps(crawler.samair())
+    try:
+        return json.dumps(crawler.samair())
+    except Exception as e:
+        return {'error': 'system error', 'msg': e.message}
 
 
 @proxy.route("/proxy/samair/shuffle")
 def samair_shuffle():
-    return json.dumps(crawler.shuffle(crawler.samair()))
+    try:
+        return json.dumps(crawler.shuffle(crawler.samair()))
+    except Exception as e:
+        return {'error': 'system error', 'msg': e.message}
 
 
 @proxy.route("/proxy/sslproxies")
 def sslproxies():
-    return json.dumps(crawler.ssl_proxies())
+    try:
+        return json.dumps(crawler.ssl_proxies())
+    except Exception as e:
+        return {'error': 'system error', 'msg': e.message}
 
 
 @proxy.route("/proxy/sslproxies/shuffle")
 def sslproxies_shuffle():
-    return json.dumps(crawler.shuffle(crawler.ssl_proxies()))
+    try:
+        return json.dumps(crawler.shuffle(crawler.ssl_proxies()))
+    except Exception as e:
+        return {'error': 'system error', 'msg': e.message}
 
 
 @proxy.route("/proxy/hidemyass")
 def hidemyass():
-    protocol = request.args.get('protocol', 'http')
-    return json.dumps(crawler.hide_my_ass(protocol))
+    try:
+        protocol = request.args.get('protocol', 'http')
+        return json.dumps(crawler.hide_my_ass(protocol))
+    except Exception as e:
+        return {'error': 'system error', 'msg': e.message}
 
 
 @proxy.route("/proxy/hidemyass/shuffle")
 def hidemyass_shuffle():
-    protocol = request.args.get('protocol', 'http')
-    return json.dumps(crawler.shuffle(crawler.hide_my_ass(protocol)))
+    try:
+        protocol = request.args.get('protocol', 'http')
+        return json.dumps(crawler.shuffle(crawler.hide_my_ass(protocol)))
+    except Exception as e:
+        return {'error': 'system error', 'msg': e.message}
